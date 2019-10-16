ff7b110219a9103a6c56c396e98914d78a07d638
Crispancho93 <34691721+Crispancho93@users.noreply.github.com>
2019-10-16 17:20:53 -0500

    16-10-19

--- db/administrativo.sql
+++ db/administrativo.sql
@@ -10,10 +10,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema administrativo
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema administrativo
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `administrativo` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci ;
 USE `administrativo` ;
 
@@ -22,33 +18,20 @@ USE `administrativo` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `administrativo`.`categoria` (
   `idcategoria` INT NOT NULL AUTO_INCREMENT,
-  `nombre_categoria` VARCHAR(45) NULL,
+  `nombre_categoria` VARCHAR(45) NULL NULL,
   `observacion` VARCHAR(100) NULL,
   PRIMARY KEY (`idcategoria`))
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
--- Table `administrativo`.`articulo`
+-- Table `administrativo`.`pago`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `administrativo`.`articulo` (
-  `idarticulo` INT NOT NULL AUTO_INCREMENT,
-  `categoria` INT NOT NULL,
-  `codigo` VARCHAR(50) NULL,
-  `nombre` VARCHAR(100) NOT NULL,
-  `stock` DECIMAL(11,2) NOT NULL,
-  `descripcion` VARCHAR(256) NULL,
-  `imagen` VARCHAR(50) NULL,
-  PRIMARY KEY (`idarticulo`),
-  UNIQUE INDEX `nombre_UNIQUE` (`nombre` ASC) ,
-  INDEX `fk_articulo_categoria_idx` (`categoria` ASC) ,
-  CONSTRAINT `fk_articulo_categoria`
-    FOREIGN KEY (`categoria`)
-    REFERENCES `administrativo`.`categoria` (`idcategoria`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `administrativo`.`pago` (
+	`idpago` INT NOT NULL AUTO_INCREMENT,
+	`llave` VARCHAR(100) NOT NULL,	
+	PRIMARY KEY (`idpago`))
 ENGINE = InnoDB;
-
+CREATE UNIQUE INDEX `llave_UNIQUE` ON pago (`llave` ASC) ;
 
 -- -----------------------------------------------------
 -- Table `administrativo`.`persona`
@@ -66,6 +49,7 @@ CREATE TABLE IF NOT EXISTS `administrativo`.`persona` (
   `correo` VARCHAR(50) NULL,
   `nombre_contacto` VARCHAR(45) NULL,
   `numero_contacto` VARCHAR(45) NULL,
+  `observacion` VARCHAR(256) NULL,
   PRIMARY KEY (`idpersona`),
   UNIQUE INDEX `num_documento_UNIQUE` (`num_documento` ASC) )
 ENGINE = InnoDB;
@@ -85,10 +69,44 @@ CREATE TABLE IF NOT EXISTS `administrativo`.`usuario` (
   `loguin` VARCHAR(20) NOT NULL,
   `clave` VARCHAR(64) NOT NULL,
   `tipo_usuario` VARCHAR(20) NOT NULL,
+  `observacion` VARCHAR(256) NOT NULL,
   PRIMARY KEY (`idusuario`),
   UNIQUE INDEX `loguin_UNIQUE` (`loguin` ASC) )
 ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `administrativo`.`rango_factura`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `administrativo`.`rango_factura` (
+	`idrango_factura` INT NOT NULL AUTO_INCREMENT,
+    `inicio` INT NOT NULL,
+    `fin` INT NOT NULL,
+    `fecha` DATETIME NULL,
+    `observacion` VARCHAR(256) NULL,
+    PRIMARY KEY(`idrango_factura`))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `administrativo`.`articulo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `administrativo`.`articulo` (
+  `idarticulo` INT NOT NULL AUTO_INCREMENT,
+  `categoria` INT NOT NULL,
+  `codigo` VARCHAR(50) NULL,
+  `nombre` VARCHAR(100) NOT NULL,
+  `stock` DECIMAL(11,2) NOT NULL DEFAULT 0,
+  `descripcion` VARCHAR(256) NULL,
+  `imagen` VARCHAR(50) NULL,
+  `observacion` VARCHAR(256) NULL,
+  PRIMARY KEY (`idarticulo`),
+  UNIQUE INDEX `nombre_UNIQUE` (`nombre` ASC) ,
+  INDEX `fk_articulo_categoria_idx` (`categoria` ASC) ,
+  CONSTRAINT `fk_articulo_categoria`
+    FOREIGN KEY (`categoria`)
+    REFERENCES `administrativo`.`categoria` (`idcategoria`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `administrativo`.`venta`
@@ -98,22 +116,23 @@ CREATE TABLE IF NOT EXISTS `administrativo`.`venta` (
   `idpersona` INT NOT NULL,
   `idusuario` INT NOT NULL,
   `idarticulo` INT NOT NULL,
+  `consecutivo` INT NOT NULL DEFAULT 0,
   `tipo_comprobante` VARCHAR(20) NOT NULL,
-  `num_comprobante` VARCHAR(10) NULL,
+  `num_comprobante` VARCHAR(45) NULL,
   `fecha_expedicion` DATETIME NULL,
   `fecha_vencimineto` DATETIME NULL,
-  `cantidad` DECIMAL(11,2) NOT NULL,
-  `valor_unitario` DECIMAL(11,2) NOT NULL,
-  `subtotal` DECIMAL(11,2) NOT NULL,
-  `descuento` DECIMAL(11,2) NULL,
-  `subtotal_descuento` DECIMAL(11,2) NULL,
-  `iva` DECIMAL(11,2) NULL,
-  `total` DECIMAL(11,2) NULL,
-  `rte_fuente` DECIMAL(11,2) NULL,
-  `rte_iva` DECIMAL(11,2) NULL,
-  `rte_ica` DECIMAL(11,2) NULL,
-  `neto` DECIMAL(11,2) NULL,
-  `observaciones` VARCHAR(100) NULL,
+  `cantidad` DECIMAL(11,2) NOT NULL DEFAULT 0,
+  `valor_unitario` DECIMAL(11,2) NOT NULL DEFAULT 0,
+  `subtotal` DECIMAL(11,2) NOT NULL DEFAULT 0,
+  `descuento` DECIMAL(11,2) NULL DEFAULT 0,
+  `subtotal_descuento` DECIMAL(11,2) NULL DEFAULT 0,
+  `iva` DECIMAL(11,2) NULL DEFAULT 0,
+  `total` DECIMAL(11,2) NULL DEFAULT 0,
+  `rte_fuente` DECIMAL(11,2) NULL DEFAULT 0,
+  `rte_iva` DECIMAL(11,2) NULL DEFAULT 0,
+  `rte_ica` DECIMAL(11,2) NULL DEFAULT 0,
+  `neto` DECIMAL(11,2) NULL DEFAULT 0,
+  `observacion` VARCHAR(256) NULL,
   PRIMARY KEY (`idventa`),
   INDEX `fk_ingreso_persona_idx` (`idpersona` ASC) ,
   INDEX `fk_ingreso_usuario_idx` (`idusuario` ASC) ,
@@ -145,10 +164,10 @@ CREATE TABLE IF NOT EXISTS `administrativo`.`ingreso` (
   `idusuario` INT NOT NULL,
   `idarticulo` INT NOT NULL,
   `tipo_comprobante` VARCHAR(20) NOT NULL,
-  `num_comprobante` VARCHAR(10) NULL,
+  `num_comprobante` VARCHAR(45) NULL,
   `fecha_expedicion` DATETIME NULL,
   `fecha_vencimineto` DATETIME NULL,
-  `valor_venta` DECIMAL(11,2) NULL,
+  `valor_venta` DECIMAL(11,2) NULL default 0,
   `cantidad` DECIMAL(11,2) NOT NULL DEFAULT 0,
   `valor_unitario` DECIMAL(11,2) NOT NULL DEFAULT 0,
   `subtotal` DECIMAL(11,2) NOT NULL DEFAULT 0,
@@ -161,7 +180,7 @@ CREATE TABLE IF NOT EXISTS `administrativo`.`ingreso` (
   `rte_ica` DECIMAL(11,2) NULL DEFAULT 0,
   `neto` DECIMAL(11,2) NULL DEFAULT 0,
   `estado` VARCHAR(45) NULL,
-  `observaciones` VARCHAR(100) NULL,
+  `observacion` VARCHAR(256) NULL,
   PRIMARY KEY (`idingreso`),
   INDEX `fk_ingreso_persona_idx` (`idpersona` ASC) ,
   INDEX `fk_ingreso_usuario_idx` (`idusuario` ASC) ,
@@ -183,16 +202,6 @@ CREATE TABLE IF NOT EXISTS `administrativo`.`ingreso` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Table `administrativo`.`pago`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `administrativo`.`pago` (
-	`idpago` INT NOT NULL AUTO_INCREMENT,
-	`llave` VARCHAR(100) NOT NULL,	
-	PRIMARY KEY (`idpago`))
-ENGINE = InnoDB;
-
-CREATE UNIQUE INDEX `llave_UNIQUE` ON pago (`llave` ASC) ;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
